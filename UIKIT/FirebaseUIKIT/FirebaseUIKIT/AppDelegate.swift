83a7d421e03168f922d829048b267e4660b16e72
cfsandovall <carlos_72_75@hotmail.com>
2025-11-18 16:52:09 -0700

    feat: Se añadio google oauth en la autenticacion en el proyecto firebase , fix: se arreglaron algunas vistas con layouts

--- UIKIT/FirebaseUIKIT/FirebaseUIKIT/AppDelegate.swift
+++ UIKIT/FirebaseUIKIT/FirebaseUIKIT/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Firebase
+import GoogleSignIn
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,8 +16,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
+        
         // Override point for customization after application launch.
         return true
+    }
+    
+    func application(_ app: UIApplication,
+                     open url: URL,
+                     options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        return GIDSignIn.sharedInstance.handle(url)
     }
 
     // MARK: UISceneSession Lifecycle
