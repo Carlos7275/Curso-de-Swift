2c407a24ee9a54def15c1bdc74143bd844d66794
Carlos Fernando Sandoval Lizárraga <68504974+Carlos7275@users.noreply.github.com>
2025-12-04 19:40:19 -0700

    App con DiffableDataSource usando Tablas y Colecciones en UIKIT (#49)
    * Proyecto de venta de libros basico en swiftui
    
    * feat: Se añadio google oauth en la autenticacion en el proyecto firebase , fix: se arreglaron algunas vistas con layouts
    
    * feat: proyecto con scrollview simulando una pantalla de registro con validaciones y spinners
    
    * feat: proyecto de escaneo de hojas y qr
    
    * Novedades IOS16, Y buscador de peliculas en SwiftUI utilizando RxSwift y Alamofire
    
    * Buscador de peliculas en uikit con adaptaciones
    
    * fix: Cambios de estructuracion de proyecto y manejo de errores, feat: pruebas unitarias y de integracion en el proyecto
    
    * feat: aplicacion de tarjetas de credito con coredata en swiftui
    
    * feat: app de ejemplo de boards y reels basica ejemplo de usar tabviews
    
    * Organizacion de proyectos por categoria
    
    * feat: app basica de carrouseles y reels en uikit
    
    * feat: aplicación de chat usando firebase en swiftui con MVVM
    
    * feat: aplicacion de chat con firebase en uikit con validaciones y celdas personalizadas
    
    * feat: aplicación de importacion y exportacion de datos a csv , html y pdf con coredata
    
    * feat: widgets en swiftui del lockscreen
    
    * feat: aplicación con swiftdata en swiftui de gestion de compras
    
    * feat: proyecto de imagenes en swiftdata en swiftui
    
    * feat: Aplicación de mapas y rutas con mapkit en swiftui
    
    * feat: app de ejemplo de utilizacion de tipkit en swiftui
    
    * feat: aplicacion wiki synthesizer con swiftui y api de wikipedia
    
    * fix(wikiSyntesizer) se elimino el mensaje fijo que estaba por error en un viewmodel, feat: Aplicacion de importacion y exportacion de SwiftData con swiftui
    
    * feat: App de ejemplo utilizando DiffableDataSource con UIKIT

---
+++ UIKIT/Basicos/DifflableDataSource/DifflableDataSource/Views/EjemploTableView/EjemploTableViewController.swift
@@ -0,0 +1,119 @@
+//
+//  EjemploTableViewController.swift
+//  DifflableDataSource
+//
+//  Created by CARLOS FERNANDO SANDOVAL LIZARRAGA on 04/12/25.
+//
+
+import UIKit
+
+class EjemploTableViewController: UIViewController, UITableViewDelegate {
+    typealias DataSource = UITableViewDiffableDataSource<Section, Message>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Section, Message>
+
+    @IBOutlet weak var tvData: UITableView!
+    private lazy var dataSource = makeDataSource()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupTableView()
+        applySnapshot()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        applySnapshot()
+    }
+
+    func makeDataSource() -> DataSource {
+        let reuseIdentifier = "ExampleTableViewCell"
+
+        return UITableViewDiffableDataSource(tableView: tvData) {
+            tableView,
+            indexPath,
+            message in
+            let cell =
+                tableView.dequeueReusableCell(
+                    withIdentifier: reuseIdentifier,
+                    for: indexPath
+                ) as! ExampleTableViewCell
+
+            cell.configure(
+                message: message.text,
+                image: message.systemImageName
+            )
+            return cell
+        }
+    }
+
+    func applySnapshot(animatingDifferences: Bool = true) {
+        var snapshot = Snapshot()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(items)
+        updateEmptyView()
+        dataSource.apply(snapshot, animatingDifferences: animatingDifferences)
+    }
+
+    func setupTableView() {
+
+        tvData.delegate = self
+
+        tvData.register(
+            UINib(nibName: "ExampleTableViewCell", bundle: nil),
+            forCellReuseIdentifier: "ExampleTableViewCell"
+        )
+
+        tvData.rowHeight = UITableView.automaticDimension
+        tvData.rowHeight = 90
+
+    }
+
+    // Swipe para la derecha (trailing)
+    func tableView(
+        _ tableView: UITableView,
+        trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath
+    ) -> UISwipeActionsConfiguration? {
+
+        guard let message = dataSource.itemIdentifier(for: indexPath) else {
+            return nil
+        }
+
+        let deleteAction = UIContextualAction(
+            style: .destructive,
+            title: "Eliminar"
+        ) { [weak self] _, _, completionHandler in
+            guard let self = self else { return }
+
+            if let index = items.firstIndex(of: message) {
+                items.remove(at: index)
+            }
+
+            self.applySnapshot()
+            completionHandler(true)
+        }
+
+        // Puedes agregar más acciones aquí
+        let configuration = UISwipeActionsConfiguration(actions: [deleteAction])
+        configuration.performsFirstActionWithFullSwipe = true
+        return configuration
+    }
+
+    private func updateEmptyView() {
+        if items.isEmpty {
+            tvData.backgroundView = emptyView
+
+            // Centrar el stack en la collectionView
+            NSLayoutConstraint.activate([
+                tvData.centerXAnchor.constraint(
+                    equalTo: tvData.centerXAnchor
+                ),
+                tvData.centerYAnchor.constraint(
+                    equalTo: tvData.centerYAnchor
+                ),
+            ])
+        } else {
+            tvData.backgroundView = nil
+        }
+    }
+
+}
